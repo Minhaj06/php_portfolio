2de5a7a3fd2feaa0bd68236ac14aba21517a26d3
Minhaj06 <milonmdminhajkobir@gmail.com>
2022-08-08 16:31:12 +0600

    Testimonials Section Started

--- admin/views/testimonials_view.php
+++ admin/views/testimonials_view.php
@@ -19,71 +19,70 @@
             </span>
         </div>
 
-        <div class="card-body" id="service_items">
-            <table id="usersDataTable" class="table table-striped table-hover">
-                <thead>
-                    <tr>
-                        <th>Reviewer Image</th>
-                        <th>Reviewer Name</th>
-                        <th>Reviewer Title</th>
-                        <th>Reviewer Comment</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
+        <div class="card-body" id="service_items"></div>
+        <table id="usersDataTable" class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th>Reviewer Image</th>
+                    <th>Reviewer Name</th>
+                    <th>Reviewer Title</th>
+                    <th>Reviewer Comment</th>
+                    <th>Actions</th>
+                </tr>
+            </thead>
+            <tbody>
 
-                    <?php
-                    $port_itmes_query = mysqli_query($conn, "SELECT * FROM `testimonial_items`");
+                <?php
+                $port_itmes_query = mysqli_query($conn, "SELECT * FROM `testimonial_items`");
 
-                    if (mysqli_num_rows($port_itmes_query) > 0) {
-                        while ($test_item_result = mysqli_fetch_array($port_itmes_query)) {
-                    ?>
-                    <tr id="<?= $test_item_result['test_item_id'] ?>">
-                        <td data-target="test_reviewer_image">
-                            <img style="width: 10rem; height: 8rem;"
-                                src="../uploaded_img/<?= $test_item_result['test_reviewer_image'] ?>"
-                                alt="Reviewer Image">
-                        </td>
+                if (mysqli_num_rows($port_itmes_query) > 0) {
+                    while ($test_item_result = mysqli_fetch_array($port_itmes_query)) {
+                ?>
+                <tr id="<?= $test_item_result['test_item_id'] ?>">
+                    <td data-target="test_reviewer_image">
+                        <img style="width: 10rem; height: 8rem;"
+                            src="../uploaded_img/<?= $test_item_result['test_reviewer_image'] ?>" alt="Reviewer Image">
+                    </td>
 
-                        <td data-target="test_reviewer_name">
-                            <?= $test_item_result['test_reviewer_name'] ?>
-                        </td>
+                    <td data-target="test_reviewer_name">
+                        <?= $test_item_result['test_reviewer_name'] ?>
+                    </td>
 
-                        <td data-target="test_reviewer_title">
-                            <?= $test_item_result['test_reviewer_title'] ?>
-                        </td>
+                    <td data-target="test_reviewer_title">
+                        <?= $test_item_result['test_reviewer_title'] ?>
+                    </td>
 
-                        <td data-target="test_reviewer_comment">
-                            <?= $test_item_result['test_reviewer_comment'] ?>
-                        </td>
+                    <td data-target="test_reviewer_comment">
+                        <?= $test_item_result['test_reviewer_comment'] ?>
+                    </td>
 
-                        <td style="min-width: 10rem;">
-                            <span data-id="<?= $test_item_result['test_item_id'] ?>" class="fs-4 text-capitalize"
-                                id="edit_testiomonial_btn" role="button" data-bs-target="#" data-bs-toggle="modal">
-                                <i class="fa-solid fa-eye"></i>
-                            </span>
+                    <td style="min-width: 10rem;">
+                        <span data-id="<?= $test_item_result['test_item_id'] ?>" class="fs-4 text-capitalize"
+                            id="edit_testiomonial_btn" role="button" data-bs-target="#" data-bs-toggle="modal">
+                            <i class="fa-solid fa-eye"></i>
+                        </span>
 
-                            <span data-id="<?= $test_item_result['test_item_id'] ?>" class="fs-4 text-capitalize ms-4"
-                                id="edit_testimonial_btn" role="button" data-bs-target="#" data-bs-toggle="modal">
-                                <i class="fa-solid fa-pen-to-square"></i>
-                            </span>
+                        <span data-id="<?= $test_item_result['test_item_id'] ?>" class="fs-4 text-capitalize ms-4"
+                            id="edit_testimonial_btn" role="button" data-bs-target="#" data-bs-toggle="modal">
+                            <i class="fa-solid fa-pen-to-square"></i>
+                        </span>
 
-                            <span data-id="<?= $test_item_result['test_item_id'] ?>" class="fs-4 text-capitalize ms-4"
-                                id="delete_testiomonial_btn" role="button">
-                                <i class="fa-solid fa-trash-can"></i>
-                            </span>
-                        </td>
-                    </tr>
-                    <?php
-                        }
+                        <span data-id="<?= $test_item_result['test_item_id'] ?>" class="fs-4 text-capitalize ms-4"
+                            id="delete_testiomonial_btn" role="button">
+                            <i class="fa-solid fa-trash-can"></i>
+                        </span>
+                    </td>
+                </tr>
+                <?php
                     }
-                    ?>
+                }
+                ?>
 
-                </tbody>
-            </table>
+            </tbody>
+        </table>
 
-        </div>
     </div>
+</div>
 
 
 </div>
